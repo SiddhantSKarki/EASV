bba6227ed4d9e39f2dcbe9f2c5d2b42efbc102bc
karkiss <karkiss@miamioh.edu>
2023-02-28 18:24:31 -0500

    Done

--- my_preprocess-gpu.sh
+++ my_preprocess-gpu.sh
@@ -35,21 +35,21 @@ python extract_feature.py \
 -i $data/features/${soundtype}/OF_10s_21.5fps \
 -o $data/features/${soundtype}/feature_rgb_bninception_dim1024_21.5fps
 
-# python extract_feature.py \
-# -t $data/filelists/${soundtype}_test.txt \
-# -m RGB \
-# -i $data/features/${soundtype}/OF_10s_21.5fps \
-# -o $data/features/${soundtype}/feature_rgb_bninception_dim1024_21.5fps
+python extract_feature.py \
+-t $data/filelists/${soundtype}_test.txt \
+-m RGB \
+-i $data/features/${soundtype}/OF_10s_21.5fps \
+-o $data/features/${soundtype}/feature_rgb_bninception_dim1024_21.5fps
 
 #Extract optical flow feature
-# python extract_feature.py \
-# -t $data/filelists/${soundtype}_train.txt \
-# -m Flow \
-# -i $data/features/${soundtype}/OF_10s_21.5fps \
-# -o $data/features/${soundtype}/feature_flow_bninception_dim1024_21.5fps
+python extract_feature.py \
+-t $data/filelists/${soundtype}_train.txt \
+-m Flow \
+-i $data/features/${soundtype}/OF_10s_21.5fps \
+-o $data/features/${soundtype}/feature_flow_bninception_dim1024_21.5fps
 
-# python extract_feature.py \
-# -t $data/filelists/${soundtype}_test.txt \
-# -m Flow \
-# -i $data/features/${soundtype}/OF_10s_21.5fps \
-# -o $data/features/${soundtype}/feature_flow_bninception_dim1024_21.5fps
+python extract_feature.py \
+-t $data/filelists/${soundtype}_test.txt \
+-m Flow \
+-i $data/features/${soundtype}/OF_10s_21.5fps \
+-o $data/features/${soundtype}/feature_flow_bninception_dim1024_21.5fps
